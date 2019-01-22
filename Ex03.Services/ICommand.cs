cbb0a1ee9643598b523755d124287042790a121e
Amir Padan <amirpadan@gmail.com>
2019-01-22 15:28:28 +0200

    Added ICommand

---
+++ Ex03.Services/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.Services
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+}
